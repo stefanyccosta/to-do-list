f7402e2f90e0ef2b2e991f6bcf53c33a12a35250
stefanyccosta <stefanycristina97.sc@gmail.com>
2026-01-29 16:39:19 -0300

    card criado

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,23 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Input } from "@/components/ui/input";
 const Home = () => {
-  return <div>Olá mundo!</div>;
+  return (
+    <main className="w-full h-screen bg-gray-100 flex justify-center items-center">
+      <Card className="w-lg">
+        <Input />
+        <Button>Cadastrar</Button>
+      </Card>
+    </main>
+  );
 };
 export default Home;
