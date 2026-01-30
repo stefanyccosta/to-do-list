d8fc21613edaad91fd88fbe2c1174ea5b2cfc42c
stefanyccosta <stefanycristina97.sc@gmail.com>
2026-01-30 15:40:26 -0300

    criação de botoes de tarefas

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,39 @@
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
+import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Plus, List, Check, CircleAlert } from "lucide-react";
+
 const Home = () => {
   return (
     <main className="w-full h-screen bg-gray-100 flex justify-center items-center">
       <Card className="w-lg">
-        <Input />
-        <Button>Cadastrar</Button>
+        <CardContent className="flex gap-2">
+          <Input placeholder="Adicionar tarefa" />
+          <Button className="cursor-pointer">
+            {" "}
+            <Plus /> Cadastrar
+          </Button>
+        </CardContent>
+        <Separator />
+        <div className="flex gap-2">
+          <Badge className="cursor-pointer">
+            {" "}
+            <List />
+            Todas
+          </Badge>
+          <Badge className="cursor-pointer">
+            {" "}
+            <CircleAlert />
+            Não finalizadas
+          </Badge>
+          <Badge className="cursor-pointer">
+            {" "}
+            <Check />
+            Concluídas
+          </Badge>
+        </div>
       </Card>
     </main>
   );
